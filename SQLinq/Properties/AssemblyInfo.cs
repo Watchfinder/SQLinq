76d2affa2a1de9103c2de59526a34388f96a5224
Jonathan Gill <jonathan@watchfinder.co.uk>
2016-04-23 09:13:26 +0100

    COnvert from PCL to standard .net and upgraded to 4.5.2

--- SQLinq/Properties/AssemblyInfo.cs
+++ SQLinq/Properties/AssemblyInfo.cs
@@ -6,21 +6,21 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SQLinq")]
-[assembly: AssemblyDescription("https://github.com/crpietschmann/SQLinq")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Chris Pietschmann (http://pietschsoft.com)")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SQLinq")]
-[assembly: AssemblyCopyright("Copyright © Chris Pietschmann 2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-//[assembly: ComVisible(false)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-//[assembly: Guid("2cfb44a2-e59e-46f1-a2da-3dade4d2878d")]
+[assembly: Guid("2d78ee41-9916-41e5-9d90-b0f3e387a985")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.1.0")]
-[assembly: AssemblyFileVersion("3.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
